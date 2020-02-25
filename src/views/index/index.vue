3e64b137f01b3f9c35e083a3b73c7c79f97005c2
123 <123@qq.com.cn>
2020-02-25 23:45:42 +0800

    左侧导航

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,19 +2,49 @@
   <el-container class="myflex">
     <el-header class="index_head">
       <div class="left">
-        <i class="el-icon-s-unfold icon_head"></i>
+        <i
+          @click="isCollapse = !isCollapse"
+          class="icon_head"
+          :class="isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"
+        ></i>
         <img src="../../assets/index_logo.png" alt />
         <span>黑马面面</span>
       </div>
       <div class="right">
         <img :src="avatar" alt />
-        <span class="userhi">{{username}},您好</span>
+        <span class="userhi">{{ username }},您好</span>
         <el-button size="mini" type="primary" @click="indexLogout">退出</el-button>
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="auto">
+        <!-- 左侧导航 -->
+        <el-menu default-active="1" class="el-menu-vertical-demo" :collapse="isCollapse">
+          <el-menu-item index="1">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+
       <el-main>Main</el-main>
+      <!-- 右侧内容 -->
     </el-container>
   </el-container>
 </template>
@@ -26,6 +56,7 @@ export default {
     return {
       avatar: "",
       username: "",
+      isCollapse: false
     };
   },
   created() {
@@ -72,7 +103,7 @@ export default {
 }
 .index_head {
   height: 60px;
-  color: #333;
+  color: #636363;
   background: #fff;
   box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
   display: flex;
@@ -82,12 +113,20 @@ export default {
   }
 }
 .el-aside {
-  background-color: #d3dce6;
-  color: #333;
+  background-color: #fff;
+  box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+  border-right: solid 1px #e6e6e6;
+  .el-menu{
+    border-right:none;
+  }
+  .el-menu-item {
+    color: #686a6e;
+    font-weight: bold;
+    font-size: 14px;
+  }
 }
 .el-main {
-  background-color: #e9eef3;
-  color: #333;
+  background-color: #f8f8f8;
 }
 .left {
   display: flex;
@@ -116,5 +155,12 @@ export default {
     color: rgba(99, 99, 99, 1);
     margin: 0 20px;
   }
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.el-menu-vertical-demo {
+  min-height: 400px;
 }
 </style>
